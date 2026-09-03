843d81bb6f3f5226b3f666940dd5541c1891ff61
Justin-aish <justinarmando988@gmail.com>
2026-09-03 19:32:45 +0700

    v1

---
+++ latihantipedata.dart
@@ -0,0 +1,34 @@
+void main() {
+  // String nama = "Justin";
+  // var nama1 = "Justin";
+  // int angka = 100;
+  // var angka1 = 100;
+
+  // String nama = "Deka";
+  // int umur = 28;
+  // double berat = 56.5;
+  // String kota=  "Pontianak";
+  // //nama saya deka, umur 28 tahun, berat56.5 , tinggal di pontianak
+  // print("Nama saya $nama , umur $umur tahun, berat $berat kg, tinggal di kota $kota  ");
+
+//string, int, double,bool
+  // int angka1 = 20;
+  // double angka2 = 20.5;
+  // num hasil = 0;
+  // hasil = angka1 + angka2;
+
+  // dynamic nama = "Wendy";
+  // dynamic angka3 = 1000;
+
+  // bool bangunpagi = true;
+  // var bangunpagi = true;
+  // dynamic bangunpagi = true;
+
+//array => dart = list, set, map
+
+  List<dynamic> mhs = ['Deny', 'Arkan', 'Çindy', 100, true, 20.5];
+  List<String> mhs1 = ['Deny', 'Arkan', 'Çindy', 'Windy', 'Jenny'];
+  print("Nama saya ${mhs[2]}");
+  List data = ['Deny', 30, 'Sistem Infornmasi'];
+  print("Nama saya ${data[0]} , umur ${data[1]}, prodi ${data[2]}");
+}
